8852c9d5abcaafff5c7f8ce8eb3f9340dcf85e62
Andrew-D-Gibson <Andrew-D-Gibson@users.noreply.github.com>
2025-07-22 08:52:40 -0400

    Added bump tile

--- Source/Content/Effects/DiceValueChangers/change_activator_to_value_effect.gd
+++ Source/Content/Effects/DiceValueChangers/change_activator_to_value_effect.gd
@@ -2,6 +2,7 @@ class_name ChangeActivatorToValueEffect
 extends Effect
 
 @export var new_value: int 
+@export var offset_from_original_value: bool = false
 
 func play(effect_variables: EffectVariables) -> void:
 	if not effect_variables.activator_die:
@@ -12,4 +13,11 @@ func play(effect_variables: EffectVariables) -> void:
 		printerr("ChangeActivatorToValueEffect doesn't have a value set!")
 		return
 	
-	await effect_variables.activator_die.reroll_with_tween(new_value)
+	if offset_from_original_value:
+		await effect_variables.activator_die.reroll_with_tween(
+			effect_variables.activator_die.value + new_value
+		)
+	else:
+		await effect_variables.activator_die.reroll_with_tween(
+			new_value
+		)
