f7a0f1f996533cf4087568e2dfeb5850bf2199c9
Andrew-D-Gibson <Andrew-D-Gibson@users.noreply.github.com>
2025-09-27 01:45:38 -0400

    Backup with some new capsule mockup art

---
+++ Source/Systems/UI/Capsule/capsule_mockup.gd
@@ -0,0 +1,34 @@
+extends Control
+
+@export var num_of_stars: int = 100
+@export var num_of_twinkling_stars: int = 25
+@export var star_pixel_scene: PackedScene
+@export var star_twinkle_scene: PackedScene
+
+var _screen_size: Vector2 = Vector2(320, 180)
+var _stars: Array[Node2D]
+
+
+func _ready() -> void:
+	# Create new stars 
+	seed('Die Fighter 42'.hash())
+	for i in range(num_of_stars):
+		_add_star(star_pixel_scene)
+
+	for i in range(num_of_twinkling_stars):
+		_add_star(star_twinkle_scene)
+		
+		
+func _add_star(star_scene: PackedScene) -> void:
+	var star = star_scene.instantiate()
+	star.z_index = -2
+	add_child(star)
+	star.global_position = Vector2(
+		randi_range(0, _screen_size.x),
+		randi_range(0, (_screen_size.y * 2)) - 180
+	)
+
+	# Randomize the opacity of the star to simulate brightness/distance
+	star.modulate = Color(1, 1, 1, randf_range(0.25, 0.75))
+	
+	_stars.append(star)
