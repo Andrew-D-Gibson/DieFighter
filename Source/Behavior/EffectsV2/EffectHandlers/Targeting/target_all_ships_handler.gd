a0b93bf730d734a9d55a52ae5a967a748b642d65
Andrew Gibson <adgibson97@gmail.com>
2026-07-29 15:07:27 -0600

    Transition Chaos Explosion Tile

--- Source/Behavior/EffectsV2/EffectHandlers/Targeting/target_all_ships_handler.gd
+++ Source/Behavior/EffectsV2/EffectHandlers/Targeting/target_all_ships_handler.gd
@@ -2,6 +2,8 @@ class_name TargetAllShipsHandler
 extends EffectHandler
 
 func apply(_data: EffectData, context: EffectContext, _engine: ScenarioEngine) -> void:
-	context.targets = Globals.enemy_manager.get_alive_enemies() as Array[Node]
+	var enemies := Globals.enemy_manager.get_alive_enemies()
+	
+	context.targets = Array(enemies, TYPE_OBJECT, "Node", null) as Array[Node]
 	if is_instance_valid(Globals.player):
 		context.targets.append(Globals.player)
